474e7cde4ae92f92769b4124e8da0d7e0e73f45f
Kerwin Wang <76610895+yukito0209@users.noreply.github.com>
2025-11-02 03:52:26 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mizuki.mysqil.com/",
+	site: "https://yukito0209.github.io",
 
 	base: "/",
 	trailingSlash: "always",
