a48660bf766f1d9f470e44578ca476b2c1b3fedb
Kerwin Wang <76610895+yukito0209@users.noreply.github.com>
2025-11-02 04:05:30 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,6 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://yukito0209.github.io",
-
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
